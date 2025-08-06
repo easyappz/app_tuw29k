9ef726c4c8615091b4c4260a0c8558dbcc7d1e46
IbragimKhaniev <newinterny@gmail.com>
2025-08-06 19:32:32 +0300

    Update files

--- react/src/pages/HomePage.js
+++ react/src/pages/HomePage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './HomePage.css';
 
-const HomePage = () => {
+function HomePage() {
   return (
     <div className="home-container">
-      <h1>Добро пожаловать в игру!</h1>
-      <p>Готовы сразиться в эпичных битвах в стиле Counter-Strike? Присоединяйтесь к нам!</p>
-      <button className="start-button">Начать игру</button>
+      <div className="home-overlay"></div>
+      <div className="home-content">
+        <h1>Добро пожаловать в 2D Counter-Strike</h1>
+        <p>Готовьтесь к битве! Соберите команду, выберите стратегию и сразитесь с противниками в этом динамичном 2D-шутере.</p>
+        <button className="play-button">Войти в игру</button>
+      </div>
     </div>
   );
-};
+}
 
 export default HomePage;
